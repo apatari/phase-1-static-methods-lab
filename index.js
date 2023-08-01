8b1aee696c486436491d7248dfd5605c138a4304
Andy Patari <andy.patari@gmail.com>
2023-08-01 13:06:24 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/gi, '')
+  }
+
+  static titleize(str) {
+    const words = str.split(" ")
+    words[0] = this.capitalize(words[0])
+    const skip = {
+      "the":'the',
+      "a":'a',
+      "an":'an',
+      "but":'but',
+      "of":'of',
+      "and":'and',
+      "for":'for',
+      "at":'at',
+      "by":'by',
+      "from":'from'
+    }
+    const res = []
+    for(const word of words) {
+      if(!(word in skip)){
+        res.push(this.capitalize(word))
+      } else {
+        res.push(word)
+      }
+    }
+    return res.join(' ')
+  }
 }
